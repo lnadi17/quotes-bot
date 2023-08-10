ad9eb94ce1d8646cac242d283f365d3f4bd7061b
Luka Nadiradze <lnadi17@freeuni.edu.ge>
2023-08-10 16:23:01 +0400

    Remove unnecessary config

--- babel.config.cjs
+++ babel.config.cjs
@@ -2,8 +2,7 @@
 module.exports = {
     env: {
         test: {
-            plugins: ["@babel/plugin-transform-modules-commonjs", "@babel/plugin-syntax-top-level-await"],
-            // presets: [['@babel/preset-env', { targets: { node: 'current' } }]]
+            plugins: ["@babel/plugin-transform-modules-commonjs"],
         }
     }
 };
